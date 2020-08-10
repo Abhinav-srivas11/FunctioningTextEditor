c01d51bbc1e17c9eecccda627a4c6fea46fad4b7
Abhinav Srivastava <abhinavsrivastava54@yahoo.com>
2020-08-10 20:23:06 +0530

    Version with upgrades and search bug fixed

--- src/com/abhinav/TextEditor.java
+++ src/com/abhinav/TextEditor.java
@@ -5,16 +5,13 @@ import javax.swing.filechooser.FileSystemView;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.time.temporal.JulianFields;
 import java.util.ArrayList;
-import java.util.*;
-import java.util.Iterator;
-import java.util.ListIterator;
-import java.util.regex.MatchResult;
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -25,17 +22,20 @@ public class TextEditor extends JFrame
     final int HEIGHT = 400;
     //these variable defined in bigger scope to allow all functions to access their values
     String foundText;
+    String loadedFile = null;
     int index = 0; // this is for getting the index of matched word from list
     ArrayList<Integer> keysList = new ArrayList<>(); // this is to store all the indexes where words were matched
+    ArrayList<String> keysListString = new ArrayList<>(); // this is to store all the indexes where words were matched
     //making the indexes into a list allows for easy iteration from next to previous search
     JTextArea textArea;
     Container container;
     JTextField filenameField;
+    JCheckBox searchWithRegex;
     int i=0; // this variable is to control which word is being focused on based on the next or previous button
-    java.util.List<String> listOfRegexMatches = new ArrayList<String>();
+
     public TextEditor()
     {
-        super("Text Editor v.2.2");
+        super("Text Editor v.2.3");
 
         setSize(WIDTH, HEIGHT);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -56,7 +56,7 @@ public class TextEditor extends JFrame
 
         //We collect all the panels for display
         container.add(scrollableTextArea, BorderLayout.CENTER);
-        container.add(contentRegion(), BorderLayout.NORTH);
+        container.add(GuiMenuRegion(), BorderLayout.NORTH);
         setJMenuBar(ourMenuBar());
         container.add(new JLabel(" "), BorderLayout.SOUTH);    //Just for beauty
         container.add(new JLabel("    "), BorderLayout.WEST);  //Just for beauty
@@ -66,9 +66,9 @@ public class TextEditor extends JFrame
     }
 
     //Top area with file name field and SAVE and LOAD buttons
-    private JPanel contentRegion(){
-        JPanel contentRegion = new JPanel();
-        contentRegion.setLayout(new FlowLayout(FlowLayout.CENTER));
+    private JPanel GuiMenuRegion(){
+        JPanel GuiMenuRegionPanelObject = new JPanel();
+        GuiMenuRegionPanelObject.setLayout(new FlowLayout(FlowLayout.CENTER));
         filenameField = new JTextField(30);
         filenameField.setName("FilenameField");
 
@@ -98,15 +98,13 @@ public class TextEditor extends JFrame
 
         //Upload a file with a name in the text box
         JButton loadButton = new JButton(new ImageIcon(((new ImageIcon(
-                ".\\src\\com\\abhinav\\images\\upload.png").getImage()
+                ".\\src\\com\\abhinav\\images\\upload_1.png").getImage()
                 .getScaledInstance(25, 25,
                         java.awt.Image.SCALE_SMOOTH))))); // this helped to scale down the image to needed size
         loadButton.setBorder(null); //this helped in removing all spaces around the logo
         loadButton.setName("OpenButton");
         //Define the function of Load Button
-        loadButton.addActionListener(actionEvent -> {
-            loadFileFunction();
-        });
+        loadButton.addActionListener(actionEvent -> loadFileFunction());
         //adding more components to the buttons and file input field
         JButton searchButton = new JButton(new ImageIcon(((new ImageIcon(
                 ".\\src\\com\\abhinav\\images\\search.png").getImage()
@@ -114,9 +112,7 @@ public class TextEditor extends JFrame
                         java.awt.Image.SCALE_SMOOTH))))); // this helped to scale down the image to needed size
         searchButton.setBorder(null); //this helped in removing all spaces around the logo
         searchButton.setName("StartSearchButton");
-        searchButton.addActionListener(search -> {
-            searchAlgorithm();
-        });
+        searchButton.addActionListener(search -> searchAlgorithm());
 
         //previous search button
         JButton previousSearchButton = new JButton(new ImageIcon(((new ImageIcon(
@@ -135,25 +131,27 @@ public class TextEditor extends JFrame
         nextSearchButton.setName("NextSearchButton");
         nextSearchButton.addActionListener(e -> front());
 
-
-
+        //Adding a regex match option to our text editor
+        searchWithRegex = new JCheckBox("Use Regex");
+        searchWithRegex.setName("MenuUseRegExp");
 
 
         // We assemble the panel from the name input field and 2 buttons
         //the order of adding the elements decides how they appear on GUI
-        contentRegion.add(saveButton);
-        contentRegion.add(loadButton);
-        contentRegion.add(filenameField);
-        contentRegion.add(searchButton);
-        contentRegion.add(previousSearchButton);
-        contentRegion.add(nextSearchButton);
+        GuiMenuRegionPanelObject.add(saveButton);
+        GuiMenuRegionPanelObject.add(loadButton);
+        GuiMenuRegionPanelObject.add(filenameField);
+        GuiMenuRegionPanelObject.add(searchButton);
+        GuiMenuRegionPanelObject.add(previousSearchButton);
+        GuiMenuRegionPanelObject.add(nextSearchButton);
+        GuiMenuRegionPanelObject.add(searchWithRegex);
 
-        return contentRegion;
+        return GuiMenuRegionPanelObject;
     }
 
     private void loadFileFunction() {
         try{
-            //jfilechooser being used to select file from the user directory
+            //JFileChooser being used to select file from the user directory
             JFileChooser loadingFile = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
             int returnValue = loadingFile.showOpenDialog(null);
 
@@ -161,6 +159,7 @@ public class TextEditor extends JFrame
             //The file chooser window will open in the middle of the user's screen
 
             if (returnValue == JFileChooser.APPROVE_OPTION) {
+                loadedFile = loadingFile.getSelectedFile().getAbsolutePath();
                 this.setTitle(loadingFile.getSelectedFile().getName());
                 textArea.setText(new String(Files.readAllBytes(Paths.get(loadingFile.getSelectedFile().getAbsolutePath()))));
             }
@@ -175,15 +174,33 @@ public class TextEditor extends JFrame
 
     private void saveFunction(){
         try {
-            JFileChooser savingFile = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
-            int returnValue = savingFile.showSaveDialog(null);
-            System.out.println(returnValue);
-            if(returnValue == JFileChooser.APPROVE_OPTION){
-                File selectedFile = savingFile.getSelectedFile();
+//            JFileChooser savingFile = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
+//            int returnValue = savingFile.showSaveDialog(null);
+
+//            if(returnValue == JFileChooser.APPROVE_OPTION){
+//                File selectedFile = savingFile.getSelectedFile();
+            File selectedFile = new File(loadedFile);
                 System.out.println(selectedFile);
                 FileWriter writer = new FileWriter(selectedFile);
                 writer.write(textArea.getText());
                 writer.close();
+//            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    private void saveAsFunction(){
+        try {
+            JFileChooser savingFile = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
+            int returnValue = savingFile.showSaveDialog(null);
+
+            if(returnValue == JFileChooser.APPROVE_OPTION){
+                File selectedFile = savingFile.getSelectedFile();
+            System.out.println(selectedFile);
+            FileWriter writer = new FileWriter(selectedFile);
+            writer.write(textArea.getText());
+            writer.close();
             }
         } catch (Exception e){
             e.printStackTrace();
@@ -197,10 +214,19 @@ public class TextEditor extends JFrame
         JMenu fileOption = new JMenu("File");
         fileOption.setMnemonic(KeyEvent.VK_F);
         fileOption.setName("MenuFile");
+
+        JMenu searchOption = new JMenu("Search");
+        searchOption.setMnemonic(KeyEvent.VK_S);
+        searchOption.setName("MenuSearch");
+
         menuBar.add(fileOption);
+        menuBar.add(searchOption);
         //above option is what we will be visible always in the GUI window
         JMenuItem saveOption = new JMenuItem("Save");
         saveOption.setName("MenuSave");
+
+        JMenuItem saveAsOption = new JMenuItem("Save As");
+//        saveOption.setName("MenuSave");
 
         JMenuItem loadOption = new JMenuItem("Load");
         loadOption.setName("MenuLoad");
@@ -208,77 +234,100 @@ public class TextEditor extends JFrame
         JMenuItem exitOption = new JMenuItem("Exit");
         exitOption.setName("MenuExit");
 
-        JMenuItem searchOption = new JMenuItem("Search");
-        saveOption.setName("MenuStartSearch");
+        JMenuItem startSearchOption = new JMenuItem("Start Search");
+        startSearchOption.setName("MenuStartSearch");
 
         JMenuItem nextSearchOption = new JMenuItem("Next Search");
-        saveOption.setName("MenuNextMatch");
+        nextSearchOption.setName("MenuNextMatch");
 
         JMenuItem previousSearchOption = new JMenuItem("Previous Search");
-        saveOption.setName("MenuPreviousMatch");
+        previousSearchOption.setName("MenuPreviousMatch");
 
-//        JMenuItem regexOption = new JMenuItem("Use Regex");
-//        saveOption.setName("MenuUseRegExp");
+        JMenuItem regexOption = new JMenuItem("Use Regex");
+        regexOption.setName("MenuUseRegExp");
 
         //add all items to the menu
         fileOption.add(saveOption);
         fileOption.add(loadOption);
-        fileOption.addSeparator();
-        fileOption.add(searchOption);
-        fileOption.add(nextSearchOption);
-        fileOption.add(previousSearchOption);
-//        fileOption.add(regexOption);
+        fileOption.add(saveAsOption);
+        searchOption.add(startSearchOption);
+        searchOption.add(nextSearchOption);
+        searchOption.add(previousSearchOption);
+        searchOption.add(regexOption);
         fileOption.addSeparator();
         fileOption.add(exitOption);
 
         //adding action listeners to each menu item
-        saveOption.addActionListener(saveEvent -> {
-            saveFunction();
-        });
+        saveOption.addActionListener(saveEvent -> saveFunction());
 
-        loadOption.addActionListener(loadEvent -> {
-            loadFileFunction();
-        });
+        saveAsOption.addActionListener(saveAsEvent -> saveAsFunction());
+
+        loadOption.addActionListener(loadEvent -> loadFileFunction());
 
         exitOption.addActionListener(exitEvent -> dispose());
+
+        startSearchOption.addActionListener(startSearch -> searchAlgorithm());
+
+        nextSearchOption.addActionListener(nextSearch -> front());
+
+        previousSearchOption.addActionListener(previousSearch -> back());
+
+        regexOption.addActionListener(useRegex -> searchWithRegex.setSelected(true));
 
         return menuBar;
     }
 
-    private boolean searchAlgorithm() {
+    private void searchAlgorithm() {
 
         foundText = filenameField.getText();
         System.out.println(foundText);
-        Pattern pattern = Pattern.compile(foundText,Pattern.CASE_INSENSITIVE);
+        Pattern pattern = Pattern.compile(foundText, Pattern.CASE_INSENSITIVE);
         Matcher match = pattern.matcher(textArea.getText());
-//        Set set = hmap.entrySet();
-//        Iterator iterator = set.iterator();
-//        Map.Entry mentry = (Map.Entry)iterator.next();
-
-        while (match.find()) {
-//            listOfRegexMatches.add((Integer.toString(match.start())));
-//            listOfRegexMatches.add(match.group());
-            keysList.add(match.start()); // this will the index to a list of keys
-        }
-//        System.out.println(listOfRegexMatches.size());
-        setFocus(0);
-        return true;
-//        Iterator iterate = list.iterator();
-//        while (iterate.hasNext()) {
-////            String s = (String) iterate.next();
-////            int index = Integer.parseInt(s);
-////            iterate.next();
-////            textArea.setCaretPosition(index + foundText.length());
-////            textArea.select(index, index + foundText.length());
-////            textArea.grabFocus();
-//            System.out.println(iterate.next());
-//            System.out.println("1"+ iterate.next());
-//        }
-//        for(int i =0; i < list.size();i+=2){
-
-//        }
+        String regex = "[a-zA-Z]+";
+        if (foundText.matches(regex)) {
+            keysList.clear();
+            keysListString.clear();
+            while (match.find()) {
+                keysList.add(match.start()); // this will the index to a list of keys
+                keysListString.add(match.group());
+            }
+            setFocus(0);
+        } else if(searchWithRegex.isSelected()){
+            keysListString.clear();
+            keysList.clear();
+            while (match.find()) {
+                keysList.add(match.start()); // this will the index to a list of keys
+                keysListString.add(match.group());
+            }
+            setFocus(0);
+        }else {
+            JOptionPane.showMessageDialog(container,
+                        "ERROR!\nNot a valid search string. Enter Again!\n ");
+            }
 
     }
+
+//    private void searching() {
+//        foundText = filenameField.getText();
+//        keysList.clear();
+//        Scanner sc2 = null;
+//        try {
+//            sc2 = new Scanner(new File(loadedFile));
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        }
+//        while (sc2.hasNextLine()) {
+//            Scanner s2 = new Scanner(sc2.nextLine());
+//            while (s2.hasNext()) {
+//                String s = s2.next();
+//                if(s.matches(foundText)){
+//                keysList.add(s2.indexOf(s));
+//            }
+//        }
+//        }
+//        setFocus(keysListString.get(0));
+//        System.out.println(keysListString.get(0));
+//    }
 
     private void front(){
         System.out.println("i value " + i);
@@ -289,13 +338,15 @@ public class TextEditor extends JFrame
         else{
             i=0;
             setFocus(0);
-    }}
+        }
+    }
 
     private void back(){
         System.out.println("i value " + i);
         if(i < 0 || i == 0){
             System.out.println("sizer"+keysList.size());
-            setFocus(0);
+            i = keysList.size()-1;
+            setFocus(i);
             System.out.println(i);}
         else {
             setFocus(--i);
@@ -305,6 +356,7 @@ public class TextEditor extends JFrame
     public void setFocus(int i){
         System.out.println(i);
         index = keysList.get(i);
+        foundText = keysListString.get(i);
         textArea.setCaretPosition(index + foundText.length());
         textArea.select(index, index + foundText.length());
         textArea.grabFocus();
